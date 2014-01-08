b6c8dda5be4084bc4c1bbdba0c15d3a7696234be
Andrew Dixon <dixon@atomicbroadcast.net>
2014-01-08 10:56:38 -0800

    kicking kata ass

--- lib/wordwrap.rb
+++ lib/wordwrap.rb
@@ -1,2 +1,44 @@
 class Wordwrap
+  def initialize wrap_length
+    if (!wrap_length.class.ancestors.include?(Integer) || wrap_length.class == Integer)
+      raise Exception "parameter must be an Integer"
+    end
+    @wrap_length = wrap_length
+  end
+
+  def wrap text
+    if (text.class != String)
+      raise Exception "expected String but paramater has class #{text.class}"
+    end
+
+    if text.empty?
+      return ""
+    end
+
+    letter_array = text.split("")
+    last_space = false
+    offset = 0
+    current_length = 0
+
+    letter_array.each_with_index do |letter, index|
+      if (letter =~ /\s/)
+        last_space = index
+      end
+      if (current_length == @wrap_length && last_space)
+        letter_array[last_space] = "\n"
+        current_length = index - last_space
+        last_space = false
+      elsif (current_length == @wrap_length)
+        letter_array.insert(index, "\n")
+        current_length = 0
+      else
+        current_length += 1
+      end
+    end
+
+    if (letter_array.last == "\n")
+      letter_array.delete_at(-1)
+    end
+    letter_array.inject(:+)
+  end
 end
--- spec/wordwrap_spec.rb
+++ spec/wordwrap_spec.rb
@@ -3,10 +3,67 @@ require 'wordwrap'
 
 describe Wordwrap do
   describe "#initialize" do
-    it "instantiates" do
-      expect {
-        Wordwrap.new
-      }.to_not raise_exception
+    subject { Wordwrap.new(wrap_length) }
+    let(:wrap_length) { 4 }
+
+    specify { expect { Wordwrap.new }.to raise_exception }
+    specify { expect { subject }.to_not raise_exception }
+
+    context "invalid parameter" do
+      let(:wrap_length) { "foo" }
+      specify { expect { subject }.to raise_exception }
+    end
+  end
+
+  describe ".wrap" do
+    subject { Wordwrap.new(wrap_length).wrap(text) }
+    let(:wrap_length) { 5 }
+    let(:text) { "this is text" }
+
+    specify { expect { subject }.to_not raise_exception }
+
+    context "invalid parameter" do
+      let(:text) { [1,2,3] }
+      specify { expect { subject }.to raise_exception }
+    end
+
+    context "empty string" do
+      let(:text) { "" }
+      it { should eq("") }
+    end
+
+    context "short word" do
+      let(:text) { "word" }
+      let(:wrap_length) { 6 }
+      it { should eq("word") }
+    end
+
+    context "word word" do
+      let(:text) { "word word" }
+      let(:wrap_length) { 5 }
+      it { should eq("word\nword") }
+    end
+
+    context "word word word" do
+      let(:text) { "word word word" }
+      let(:wrap_length) { 5 }
+      it { should eq("word\nword\nword") }
+
+      context "wrap at 10" do
+        let(:wrap_length) { 10 }
+        it { should eq("word word\nword") }
+      end
+
+      context "wrap at 3" do
+        let(:wrap_length) { 3 }
+        it { should eq("wor\nd\nwor\nd\nwor\nd") }
+      end
+    end
+
+    context "wordword" do
+      let(:wrap_length) { 4 }
+      let(:text) { "wordword" }
+      it { should eq("word\nword") }
     end
   end
 end
